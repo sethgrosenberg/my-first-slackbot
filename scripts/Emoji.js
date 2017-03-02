3da0a172673763bba9010377df617bb98dd01830
Seth Rosenberg <sethgrosenberg@gmail.com>
2017-03-01 19:40:31 -0800

    Adding all the happy emojis

--- scripts/Emoji.js
+++ scripts/Emoji.js
@@ -11,9 +11,10 @@ var loveEmoji = ':heart_eyes:'
 var angelEmoji = ':angel:'
 var laughingEmoji = ':laughing:'
 var confusedEmoji = ':confounded:'
-var comicBook = 
+var snacks = 
+[ 
 {
-    "text": "New comic book alert!",
+    "text": "Cookies!",
     "attachments": [
         {
             "title": "The Further Adventures of Slackbot",
@@ -43,29 +44,51 @@ var comicBook =
             "callback_id": "comic_1234_xyz",
             "color": "#3AA3E3",
             "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "recommend",
-                    "text": "Recommend",
-                    "type": "button",
-                    "value": "recommend"
-                },
-                {
-                    "name": "no",
-                    "text": "No",
-                    "type": "button",
-                    "value": "bad"
-                }
-            ]
         }
     ]
-};
+};,
+
+{
+    "text": "Veggies + Dip!",
+    "attachments": [
+        {
+            "title": "The Further Adventures of Slackbot",
+            "fields": [
+                {
+                    "title": "Volume",
+                    "value": "1",
+                    "short": true
+                },
+                {
+                    "title": "Issue",
+                    "value": "3",
+            "short": true
+                }
+            ],
+            "author_name": "Stanford S. Strickland",
+            "author_icon": "http://a.slack-edge.com/7f18https://a.slack-edge.com/bfaba/img/api/homepage_custom_integrations-2x.png",
+            "image_url": "http://i.imgur.com/OJkaVOI.jpg?1"
+        },
+        {
+            "title": "Synopsis",
+            "text": "After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."
+        },
+        {
+            "fallback": "Would you recommend it to customers?",
+            "title": "Would you recommend it to customers?",
+            "callback_id": "comic_1234_xyz",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+        }
+    ]
+};,
+
+
 
 module.exports = function(bot) {
   bot.hear(/I am (.*)/, function(msg) {
     var emotion;
     emotion = msg.match[1];
-    console.log(emotion);
     switch (emotion) {
       case "happy":
         return msg.send(comicBook);
